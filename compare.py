4ddd44500cbc532ecbd9b825b08de0d282b2e8ff
root <root@DESKTOP-7TKI2I7.localdomain>
2019-10-23 15:41:01 -0500

    Upated plot looks.

--- compare.py
+++ compare.py
@@ -4,6 +4,7 @@ import matplotlib.patches as mpatches
 import spiceypy.spiceypy as sp
 import numpy as np
 
+
 import combine_A
 import read_B
 
@@ -25,26 +26,26 @@ plt.switch_backend('agg')
 f, ((ax1,ax2,ax3, ax4)) = plt.subplots(4,1,sharex=True)
 
 num_points = 1
-[T1A,Tf1A,C1A, Q1A] = combine_A.combine('SCA_1A/6-26/SCA1A_2019-06-26_C_04.txt',num_points)
+[T1A,Tf1A,C1A, Q1A] = combine_A.combine('SCA_1A/8-23/SCA1A_2019-08-23_C_04.txt',num_points)
 print('Combined A')
-[T1B,C1B, Q1B] = read_B.read('SCA_1B/6-26/SCA1B_2019-06-26_C_04.txt',num_points)
+[T1B,C1B, Q1B] = read_B.read('SCA_1B/8-23/SCA1B_2019-08-23_C_04.txt',num_points)
 print('Read B')
 
 #define a color for each combination type
 colorsA = []
 for i in range(0,len(Q1A)):
     if C1A[i] == '1':
-        colorsA.append('yellow')
+        colorsA.append('pink')
     elif C1A[i] == '2':
-        colorsA.append('yellow')
+        colorsA.append('black')
     elif C1A[i] == '12':
-        colorsA.append('red')
+        colorsA.append('purple')
     elif C1A[i] == '3':
         colorsA.append('yellow')
     elif C1A[i] == '13':
-        colorsA.append('red')
-    elif C1A[i] == '23':
         colorsA.append('orange')
+    elif C1A[i] == '23':
+        colorsA.append('red')
     elif C1A[i] == '123':
         colorsA.append('blue')
 
@@ -81,7 +82,7 @@ for i in range(0,len(Q1A)):
     elif C1B[i] == '23':
         colorsB.append('blue') #1,2,3,IMU
 
-num_points = int(len(Q1A)/24) - 1
+num_points = int(len(Q1A)/16) - 1
 M1A = []
 M1B = []
 eul_x = []
@@ -129,56 +130,71 @@ for i in range(num_points):
             num_err[i] +=1
             sum_err[i] += eul[i]
 #file.close()
+x_std = np.std(eul_x)
+x_mean = np.mean(eul_x)
+y_std = np.std(eul_y)
+y_mean = np.mean(eul_y)
+z_std = np.std(eul_z)
+z_mean = np.mean(eul_z)
+print('X mean: ',x_mean)
+print('X std: ',x_std)
+print('Y mean: ',y_mean)
+print('Y std: ',y_std)
+print('Z mean: ',z_mean)
+print('Z std: ',z_std)
 
-
-ax1.scatter([i for i in range(num_points)],eul_x,s = .05,c=colorsB)
-ax2.scatter([i for i in range(num_points)],eul_y,s = .05,c=colorsB)
-ax3.scatter([i for i in range(num_points)],eul_z,s = .05,c=colorsB)
-ax4.scatter([i for i in range(num_points)],theta,s = .05,c=colorsB)
+ax1.scatter([i for i in range(num_points)],eul_x,s = .05,c=colorsA)
+ax2.scatter([i for i in range(num_points)],eul_y,s = .05,c=colorsA)
+ax3.scatter([i for i in range(num_points)],eul_z,s = .05,c=colorsA)
+ax4.scatter([i for i in range(num_points)],theta,s = .05,c=colorsA)
 
 
 #legend
-red = mpatches.Patch(color='red',label='1-3')
+pink = mpatches.Patch(color='pink',label='1')
+black = mpatches.Patch(color='black',label='2')
+red = mpatches.Patch(color='red',label='2-3')
+yellow = mpatches.Patch(color='yellow',label='3')
+orange = mpatches.Patch(color='orange',label='1-3')
+purple = mpatches.Patch(color='purple',label='1-2')
 blue = mpatches.Patch(color='blue',label='1-2-3')
-ax1.legend(handles=[red,blue],loc=1,prop={'size':6})
+
+ax1.legend(handles=[pink,black,red,yellow,orange,purple,blue],loc=1,prop={'size':3})
 
 #Scaling and labeling
-
-'''
-ax1_max = .667
-ax2_max = 0
-ax3_max = 1
-ax4_max = 1
-ax1_min = -ax1_max
-ax2_min = -1
-ax3_min = -1
-ax4_min = 0
-
-ax1_range = np.ndarray.tolist(np.linspace(ax1_min,ax1_max,7))
-ax2_range = np.ndarray.tolist(np.linspace(ax2_min,ax2_max,7))
-ax3_range = np.ndarray.tolist(np.linspace(ax3_min,ax3_max,7))
-ax4_range = np.ndarray.tolist(np.linspace(ax4_min,ax4_max,7))
-#ax1.tick_params(ax1_range)
-#ax2.yticks(ax2_range)
-#ax3.yticks(ax3_range)
-#ax4.yticks(ax4_range)
-
-#ax1.grid(axis='y')
-#ax2.grid(axis='y')
-#ax3.grid(axis='y')
-#ax4.grid(axis='y')
-'''
-ax1.set_title('Euler X')
+sz = 8
+vert_shift = .92
+ax1.set_title('X',size=sz,y = vert_shift)
 ax1.set_ylim((-.667,.667))
-ax2.set_title('Euler Y')
-ax2.set_ylim((-1,0))
-ax3.set_title('Euler Z')
+ax1.yaxis.set_minor_locator(mpl.ticker.AutoMinorLocator())
+ax1.yaxis.grid(True,which='minor',linewidth=.5,linestyle='--')
+ax1.yaxis.grid(True,which='major',linewidth=.5,linestyle='--')
+
+ax2.set_title('Y',size=sz,y = vert_shift)
+ax2.set_ylim((-1,1))
+ax2.yaxis.set_minor_locator(mpl.ticker.AutoMinorLocator())
+ax2.yaxis.grid(True,which='minor',linewidth=.5,linestyle='--')
+ax2.yaxis.grid(True,which='major',linewidth=.5,linestyle='--')
+
+ax3.set_title('Z',size=sz,y = vert_shift)
 ax3.set_ylim((-1,1))
-ax4.set_title('Theta')
-ax4.set_ylim((0,1))
+ax3.yaxis.set_minor_locator(mpl.ticker.AutoMinorLocator())
+ax3.yaxis.grid(True,which='minor',linewidth=.5,linestyle='--')
+ax3.yaxis.grid(True,which='major',linewidth=.5,linestyle='--')
+
+ax4.set_title('Angle',size=sz,y = vert_shift)
+ax4.set_ylim((.2,1))
+ax4.yaxis.set_minor_locator(mpl.ticker.AutoMinorLocator())
+ax4.yaxis.grid(True,which='minor',linewidth=.5,linestyle='--')
+ax4.yaxis.grid(True,which='major',linewidth=.5,linestyle='--')
+
+
+ax1.set_ylabel('mrad',size = sz)
+ax2.set_ylabel('mrad',size = sz)
+ax3.set_ylabel('mrad',size = sz)
+ax4.set_ylabel('mrad',size = sz)
 
 plt.xlabel('Time')
-plt.ylabel('mrad')
-plt.savefig('AvsB_One_Hour.png',dpi=1000)
+ax1.set_ylabel('mrad')
+plt.savefig('AvsB_One_Orbit.png',dpi=1000)
 plt.show()
 print('Plotted and saved figure.')
